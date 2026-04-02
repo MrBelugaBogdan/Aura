c66abfad51e0029643dded33007a7e1ecb9eea74
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 14:41:34 +0300

    Refactor keyboard driver and add key mapping

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,23 +1,21 @@
-// drivers/keyboard.c - Перший драйвер AuraOS
-
 #include "../include/aura.h"
 
-// Функція читання з порту (спілкування з залізом)
-unsigned char inb(unsigned short port) {
-    unsigned char ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
+// Таблиця символів (спрощена)
+unsigned char keyboard_map[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', '-', '=', '\b',
+  '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z',
+  'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
+};
 
-// Головна функція опитування клавіатури
 void check_keyboard() {
-    // 0x64 - це порт статусу клавіатури в ПК
     if (inb(0x64) & 0x01) {
-        unsigned char scancode = inb(0x60); // Отримуємо код натиснутої клавіші
+        unsigned char scancode = inb(0x60);
         
-        // Якщо натиснуто клавішу 'Q' (скан-код 0x10), виводимо щось
-        if (scancode == 0x10) {
-            // Тут пізніше додамо вивід на екран
+        // Якщо скан-код менше 128 і це не "відпускання" клавіші
+        if (scancode < 128 && keyboard_map[scancode] != 0) {
+            char c = keyboard_map[scancode];
+            put_char(c); // Відправляємо букву на екран!
         }
     }
 }
