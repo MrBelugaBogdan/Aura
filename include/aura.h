9ad052e83f989c2e45fadc8a8562e797d855da88
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 21:41:37 +0300

    Update aura.h

--- include/aura.h
+++ include/aura.h
@@ -1,20 +1,26 @@
 #ifndef AURA_H
 #define AURA_H
 
-// Налаштування екрана та друку
+// --- Екран та друк ---
 void print_char(char c, int row, int col, char color);
 void print_string(char* str, int row, int col, char color);
 void put_char(char c);
 void clear_screen();
-extern int cursor_y; 
+extern int cursor_y;
+extern int cursor_x;
 
-// Робота з залізом (Клавіатура)
-void check_keyboard();
+// --- Залізо (Порти та Клавіатура) ---
 unsigned char inb(unsigned short port);
+void check_keyboard();
 
-// Логіка системи (Shell)
+// --- Ядро та Shell ---
 void start_shell();
 void shell_input(char c);
+void execute_command(char* cmd);
 int compare_strings(char* s1, char* s2);
+
+// --- Безпека та RAM-диск ---
+void read_secret();
+extern int access_level;
 
 #endif
