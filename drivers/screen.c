3280630a4937d82aa594a949e0fceb8b8438323b
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 21:31:53 +0300

    Add string comparison function

--- drivers/screen.c
+++ drivers/screen.c
@@ -30,3 +30,11 @@ void put_char(char c) {
         }
     }
 }
+int compare_strings(char* s1, char* s2) {
+    int i = 0;
+    while (s1[i] == s2[i]) {
+        if (s1[i] == '\0') return 1;
+        i++;
+    }
+    return 0;
+}
