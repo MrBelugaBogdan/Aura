e9ee50220e8a5a0282d281c1737d1662809864fb
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 21:43:03 +0300

    Add inb function and update keyboard input handling

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,21 +1,23 @@
 #include "../include/aura.h"
 
-// Таблиця символів (спрощена)
+unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
 unsigned char keyboard_map[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', '-', '=', '\b',
-  '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z',
-  'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
 };
 
 void check_keyboard() {
     if (inb(0x64) & 0x01) {
         unsigned char scancode = inb(0x60);
-        
-        // Якщо скан-код менше 128 і це не "відпускання" клавіші
         if (scancode < 128 && keyboard_map[scancode] != 0) {
-            char c = keyboard_map[scancode];
-            shell_input(c);
+            shell_input(keyboard_map[scancode]);
         }
     }
 }
