67208710d8b82962f8a9be34326d53a432e194ad
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 14:32:57 +0300

    Declare functions in aura.h
    Add function declarations for screen and keyboard operations.

--- include/aura.h
+++ include/aura.h
@@ -1,1 +1,9 @@
+#ifndef AURA_H
+#define AURA_H
 
+// Оголошуємо функції, щоб файли "бачили" один одного
+void print_char(char c, int row, int col, char color);
+void clear_screen();
+void check_keyboard();
+
+#endif
