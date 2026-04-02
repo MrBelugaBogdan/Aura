584e8ea5f4cac2a48ab29303d32e52112c8101cd
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-04-02 21:36:54 +0300

    Update aura.h with new function declarations
    Added function declarations for string comparison and shell input handling.

--- include/aura.h
+++ include/aura.h
@@ -1,12 +1,20 @@
 #ifndef AURA_H
 #define AURA_H
 
-// Оголошуємо функції, щоб файли "бачили" один одного
+// Налаштування екрана та друку
 void print_char(char c, int row, int col, char color);
+void print_string(char* str, int row, int col, char color);
+void put_char(char c);
 void clear_screen();
+extern int cursor_y; 
+
+// Робота з залізом (Клавіатура)
 void check_keyboard();
+unsigned char inb(unsigned short port);
+
+// Логіка системи (Shell)
+void start_shell();
+void shell_input(char c);
+int compare_strings(char* s1, char* s2);
 
 #endif
-void print_string(char* str, int row, int col, char color);
-void start_shell();
-void put_char(char c);
